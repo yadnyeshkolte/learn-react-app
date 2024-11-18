5708149a511bbf1a62f75de308e6006b53b97701
yadnyeshkolte <yadnyeshkolte@gmail.com>
2024-11-18 11:56:48 +0530

    App.test.js update, act problem is updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { act } from "react";
 
 test("renders My new React App link", () => {
-  render(<App />);
+  act(() => {
+    render(<App />);
+  });
+  //render(<App />);
   const linkElement = screen.getByText(/My new React App/i);
   expect(linkElement).toBeInTheDocument();
 });
